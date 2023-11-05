fd7dba837e42444adac0f25363e6ad61d9ffabcb
GeorgieHow <georgiehowarthz@gmail.com>
2023-11-05 21:37:41 +0000

    Added columns needed to comments migration.

--- database/migrations/2023_11_05_213128_create_comments_table.php
+++ database/migrations/2023_11_05_213128_create_comments_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('comment_text');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('post_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onDelete('cascade')->onUpdate('cascade');
+
+            $table->foreign('post_id')->references('id')->on('posts')
+            ->onDelete('cascade')->onUpdate('cascade');
+
             $table->timestamps();
         });
     }
