075ac70f0f750def1c291728f522f45be9d7f955
GeorgieHow <georgiehowarthz@gmail.com>
2023-12-08 20:14:12 +0000

    Clicking on posted by takes you to user info.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,9 @@
 @section('content')
     <ul style = "color:#ffffff;">
         <li>Post Title: {{$post->post_title}} </li>
-        <li>Posted by: {{$post->user->name }} </li>
+        <a href="{{ route('users.show', ['id' => $post -> user_id]) }}">
+            <li>Posted by: {{$post->user->name }} </li>
+        </a>
         <li>Content: {{$post->post_text}} </li>
         <li>[Created at {{$post ->created_at}}] </li>
         <li><a href="{{route('comments.create')}}" style="color:white;">
