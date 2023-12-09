c8cf3c70a39bd57816b33afce87d5ee5c70dd2c1
GeorgieHow <georgiehowarthz@gmail.com>
2023-12-09 00:07:09 +0000

    Added a users posts to their profile.

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -4,14 +4,22 @@
 
 @section('content')
 
-    <form method="POST" action = "{{route('comments.store',
+    <form wire:submit.prevent="update" method="POST" action = "{{route('comments.store',
     ['id' => $post]) }}">
         @csrf
         <ul style = "color:#ffffff;">
             <li>Comment Text: <input style="color:black;" type="text" 
                 name="comment_text" value="{{ old('comment_text')}}"/></li>
-            <input type = "submit" value = "Submit"/>
 
+                @if (session() ->has('message'))
+                <div class="alert alert-success">
+                    {{ session('message') }}
+                </div>
+                @endif  
+
+            <input type = "submit" value = "Submit"/>
+            
+              
         </ul>
         <a href="{{route('posts.show', ['id' => $post])}}" style="color:white;">Cancel</a>
     </form>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,5 +6,22 @@
     <ul style = "color:#ffffff;">
         <li>Name: {{$user->name}} </li>
         <li>Email: {{$user->email}} </li>
+        <li>Bio: {{$user->profile->profile_bio}}</li>
     </ul>
+
+    <ul>
+    <br/>
+        <li>User's Posts</li>
+        @foreach($user->posts as $post)
+            <li>
+                <a href="{{ route('posts.show', ['id' => $post -> id]) }}">
+                    {{$post ->post_title }}: 
+                    {{$post ->post_text}} 
+                    [{{$post ->created_at}}]
+                </a>
+            </li>
+            <br/>
+        @endforeach
+    </ul>
+
 @endsection
